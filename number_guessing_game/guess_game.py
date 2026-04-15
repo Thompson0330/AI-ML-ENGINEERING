6ffcc14fc1534776c0563bdfc22e2bd16a1fe706
Thompson <thompsongiweze7@gmail.com>
2026-04-15 13:14:05 +0100

    update game with 1-10 range check

--- number_guessing_game/guess_game.py
+++ number_guessing_game/guess_game.py
@@ -1,9 +1,12 @@
 import random
-secret_number= random.randint(1,10)
+secret_number=random.randint(1,10)
 while True:
-    guess=int(input("guess a number between 1 and 10: "))
-    if guess== secret_number:
-        print("correct")
-        break #stops the loop
-    else :
-        print ("wrong! the number was",secret_number, "try again!")
+    guess=int(input("guess what the actual number is: "))
+    if 1<= guess <=10:
+        if guess== secret_number:
+            print("correct")
+            break #stops the loop
+        else:
+            print ("wrong! try again")
+    else:
+        print("out of range")
